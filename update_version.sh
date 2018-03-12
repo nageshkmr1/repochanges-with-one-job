f051812e63dee2cb881647718fa7a53faf501804
nageshkmr1 <subrato.mozumdar@cbre.com>
2018-03-11 22:09:00 -0500

    added some changes on script

--- update_version.sh
+++ update_version.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-repos=( "git@github.com:nageshkmr1/test1.git"
-        "git@github.com:nageshkmr1/test2.git" )
-for i in "${repos[@]}"
-do 
-   NEW_UUID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 7 | head -n 1) 
-ssh -i "$GIT_SSH_KEYS_PATH" git clone -b  development --single-branch $i  clone-git-repo/clone_$NEW_UUID
- done
+repos=( "https://github.com/nageshkmr1/test1.git"
+        "https://github.com/nageshkmr1/test2.git" )
 
+for i in "${repos[@]}"
+do
+   NEW_UUID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 7 | head -n 1)
+   git clone -b  development --single-branch $i  clone-git-repo/clone_$NEW_UUID
+ done
 
 apt-get install npm
 npm install
@@ -19,4 +19,3 @@ mv commit.sh clone-git-repo/
 cd clone-git-repo/
 chmod +x commit.sh
 ./commit.sh
-
